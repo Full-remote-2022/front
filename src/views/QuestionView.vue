d2df1dac588a6e25d6301fd142d93c3442837ec0
Aekhy <102717242+Aekhy@users.noreply.github.com>
2022-12-01 22:40:44 +0100

    A bit of css for questions

--- src/views/QuestionView.vue
+++ src/views/QuestionView.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
         <h2>{{question.questionText}}</h2>
-        <AnswerComponent v-for="answer in question.answers" :key="answer.answerID" :answer="answer"></AnswerComponent>
+        <section>
+        <AnswerComponent class="answer" v-for="answer in question.answers" :key="answer.answerID" :answer="answer"></AnswerComponent>
+        </section>
     </div>
 </template>
 <script setup lang="ts">
@@ -39,6 +41,61 @@ let question = ref<Question>({
 })
 
 </script>
+
 <style scoped lang="scss">
-    
+div {
+    display: flex;
+    flex-direction: column;
+
+    border: $div_border;
+    border-radius: $div_border_radius;
+
+    background-color: $background_color_question;
+}
+
+h2{ 
+    background-image: linear-gradient(
+	to right,/* to add in parameter*/
+	#462523 0, /* to add in parameter*/
+    #cb9b51 22%, /* to add in parameter*/
+	#f6e27a 45%,/* to add in parameter*/
+	#f6f2c0 50%,/* to add in parameter*/
+	#f6e27a 55%,/* to add in parameter*/
+	#cb9b51 78%,/* to add in parameter*/
+	#462523 100%/* to add in parameter*/
+	);
+   color:transparent;/* to add in parameter*/
+   -webkit-background-clip:text;/* to add in parameter*/
+
+   -webkit-text-stroke-width: 1px; /* to add in parameter*/
+   -webkit-text-stroke-color: black; /* to add in parameter*/
+}
+
+section{
+    background-color: $background_color_answers;
+
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+}
+
+.answer {
+    background-color: $background_color_oneanswer;
+    color:$color_oneanswer;
+    border: $border_oneanswer;
+    border-radius: $border_radius_oneanswer;
+
+    padding: $padding_oneanswer;
+    margin: $margin_oneanswer;
+
+    transition: $transition_oneanswer ; 
+    box-shadow: $shadow_oneanswer;
+
+    &:hover {
+    transform: $transforme_oneanswer; /* to add in parameter*/
+}
+
+}
+
+
 </style>
