4ff2f3201c463c02f094ddf3b2e090ad53c747cc
Hugo REY <hrey222@gmail.com>
2022-12-01 21:38:43 +0000

    working individual cards

---
+++ src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+    <nav>
+        <p>LOGO</p>
+        <router-link to="/question">Question</router-link>
+        <router-link to="/collection">Collection</router-link>
+        <router-link to="/play">Play</router-link>
+    </nav>
+</template> 
+<script setup lang="ts">
+
+</script>
+<style lang="scss" scoped>
+nav {
+    display: flex;
+    justify-content: space-between;   
+    align-items: center;
+    margin: 0 1rem;
+}
+
+nav a {
+    font-weight: bold;
+    color: $dark;
+    text-decoration: none;
+}
+
+nav a.router-link-exact-active {
+    color: $highlight;
+}
+</style>
